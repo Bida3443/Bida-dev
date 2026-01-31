772f143eb7399a8f9faeb7cb2f96d11e87e08fd2
Bida3443 <alexanderbamise@gmail.com>
2026-01-31 07:45:26 -0800

    My Porfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header/>
+        <StairTransition/>
         <PageTransition>{children}</PageTransition>
         
       </body>
