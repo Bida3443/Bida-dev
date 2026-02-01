3772ea81e6c1196d1af4a2a7612c38778190aa8d
Bida3443 <alexanderbamise@gmail.com>
2026-02-01 15:32:30 -0800

    My Porfolio

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -4,9 +4,67 @@
 
 import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
+import {CiMenuFries} from "react-icons/ci";
+
+
+const links =[
+    {
+        name: 'home',
+        path: '/'
+    },
+    {
+        name: 'about',
+        path: '/about'
+    },
+    {
+        name: 'my-Story',
+        path: '/story'
+    },
+    {
+        name: 'skills & services',
+        path: '/service'
+    },
+    {
+        name: 'resume',
+        path: '/resume'
+    },
+    {
+        name: 'contact',
+        path: '/contact'
+    },
+]
+
+
 
 const MobileNav = () =>{
-    return <div>MobileNav</div>
+    const pathname = usePathname();
+    return <Sheet>
+        <SheetTrigger className="flex justify-center items-center">
+            <CiMenuFries className="text-[32px] text-blue-700"/>
+        </SheetTrigger>
+        <SheetContent className="flex flex-col">
+            <div className="mt-32 mb-40 text-center text-2xl">
+                <Link href="/">
+                <h1 className="text-4xl font-semibold">
+                    BiDa <span className="text-blue-700">.</span>
+                </h1>
+                </Link>
+            </div>
+
+            <nav className="flex flex-col justify-center items-center gap-8">
+                {links.map((link, index) =>{
+                    return (
+                        <Link href={link.path} key={index} className={`text-xl capitalize hover:text-blue-700 transition-all`}
+                        >
+                            {link.name}
+                        </Link>
+                    )
+                })}
+
+            </nav>
+        </SheetContent>
+    </Sheet>
 };
 
 
