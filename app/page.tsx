32142d3078c2ecfbebe0e6ba57aeabfd3906831e
Bida3443 <alexanderbamise@gmail.com>
2026-02-02 06:19:52 -0800

    My Porfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import {FiDownload} from "react-icons/fi";
-
+import social from "@/components/social";
 
 export default function Home() {
   return (
@@ -12,8 +12,17 @@ export default function Home() {
             
             <h1 className="h1 mb-6 text-blue-700 ">Idowu Alexander Bamise</h1>
             <span className="text-xl mb-20">I'm Front end developer/designer</span>
-            <p className="max-w-125 mb-9 mt-4 text-white/80">I excel at crafting elegant digital experience and i am proficient in various programming languages and technologies</p>
-            
+            <p className="max-w-125 mb-9 mt-4 text-white/80 text-justify">I excel at crafting elegant digital experience and i am proficient in various programming languages and technologies</p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button 
+              variant="outline"
+              size="lg"
+              className="uppercase flex items-center gap-2 hover:text-white hover:bg-blue-700">
+                <span>Download CV</span>
+                <FiDownload className="text-xl"/>
+              </Button>
+              <div className="mb-8 xl:mb-0">socials</div>
+            </div>
           </div>
           <div>photo</div>
         </div>
