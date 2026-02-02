ee6cb376e550f5c93e38783d402e5e0dd96e0faf
Bida3443 <alexanderbamise@gmail.com>
2026-02-02 04:03:42 -0800

    My Porfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 // import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import {FiDownload} from "react-icons/fi";
+
 
 export default function Home() {
   return (
-    <div >homepage
-    </div>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row md:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div>text</div>
+          <div>photo</div>
+        </div>
+      </div>
+
+    </section>
     
   );
 }
