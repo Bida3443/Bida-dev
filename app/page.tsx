db44ee4204205d85a0debe13caac15f2d689d3ee
Bida3443 <alexanderbamise@gmail.com>
2026-02-02 13:30:56 -0800

    My Porfolio

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Photo from "@/components/Photo";
 export default function Home() {
   return (
     <section className="h-full">
-      <div className="container mx-auto mt-20 h-full">
+      <div className="container mx-auto  h-full">
         <div className="flex flex-col xl:flex-row md:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           <div className="text-center xl:text-left">
             
--- components/Photo.tsx
+++ components/Photo.tsx
@@ -1,23 +1,29 @@
+"use client"
+
 import {motion} from "framer-motion"
 import Image from "next/image"
-import Bida from "../../Bida-dev/public/Bida.png"
+import Bida from "../public/Bidaa.png";
 
 
 export default function Photo () {
     return(
         <div className="w-full h-full relative">
             <motion.div>
-            <div className=" xl:">
-                <Image
-                priority
-                quality={100}
-                fill
-                src={Bida}
-                alt=""
-                
-                />
-            </div>
-        </motion.div>
+                <div className="w-100 h-100">
+                    <Image
+            src={Bida}
+            // fill
+            alt="Bida"
+            // width={400}
+            // height={100}
+            className=""
+            priority
+            quality={100}
+            fill
+            />
+                </div>
+            </motion.div>
+            
         </div>
     )
 }
