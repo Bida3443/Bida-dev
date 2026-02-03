9b4d16ed06fd84eec72d8152d7b378df97c63c12
Bida3443 <alexanderbamise@gmail.com>
2026-02-03 01:52:55 -0800

    My Porfolio

--- components/HeroTitle.tsx
+++ components/HeroTitle.tsx
@@ -4,9 +4,9 @@ import { Typewriter } from "react-simple-typewriter";
 
 export default function HeroTitle() {
   return (
-    <h1 className="lg:text-3xl text-2xl font-bold">
+    <h1 className="lg:text-3xl text-[22px] font-bold">
       I'm a creative {" "}
-      <span className="text-blue-700 border-b-2 border-white">
+      <span className="text-blue-500 border-b-2 border-white">
         <Typewriter
           words={[
             "Frontend Developer",
