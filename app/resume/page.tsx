8d858972188493bd25a43e4ab6ad723c424bc0d7
Bida3443 <alexanderbamise@gmail.com>
2026-02-05 04:28:15 -0800

    My Porfolio

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,11 +1,98 @@
 "use client"
 
 
+import { icons } from "lucide-react";
+import { title } from "process";
 import {FaHtml5,FaCss3, FaJs, FaReact, FaFigma, FaNodeJs,} from "react-icons/fa";
 
 import {SiTailwindcss, SiNextdotjs} from "react-icons/si";
 
+const about = {
+    title:"About me",
+    Description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, iste impedit! Commodi repellat magnam et eos in quam placeat, unde maxime? Quasi vel similique, ex illo nisi mollitia. Quos, doloribus.",
 
+    info: [
+        {
+            fieldName: "Name",
+            fieldValue: "Idowu Alexander Bamise"
+        },
+        {
+            fieldName: "Phone",
+            fieldValue: "(+234 810 281 3443)"
+        },
+        {
+            fieldName: "Experience",
+            fieldValue: "5+ Years"
+        },
+        {
+            fieldName: "Email",
+            fieldValue: "Alexanderbamise@gmail.com"
+        },
+        {
+            fieldName: "Nationality",
+            fieldValue: "Nigerian"
+        },
+        {
+            fieldName: "Frelance",
+            fieldValue: "Available"
+        },
+        {
+            fieldName: "Languages",
+            fieldValue: "English, French"
+        },
+        
+
+    ]
+    
+};
+
+const experience = {
+    icon: "/public/Bida.png",
+    title: "My experience",
+    Description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, iste impedit! Commodi repellat magnam et eos in quam placeat, unde maxime? Quasi vel similique, ex illo nisi mollitia. Quos, doloribus.",
+    items: [
+        {
+            Company: "AmondSouls",
+            Position: "Freelance Website Designer",
+            Duration: "June2022-August2022",
+        },
+         {
+            Company: "Federal University Of Technology Akure",
+            Position: "Website Developer",
+            Duration: "2023-2024",
+        },
+        {
+            Company: "Premium Smile Dental Clinic",
+            Position: "Website Developer",
+            Duration: "2025-2026",
+        },
+    ]
+
+}
+
+const education = {
+    icon: "/public/Bida.png",
+    title: "My education",
+    Description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, iste impedit! Commodi repellat magnam et eos in quam placeat, unde maxime? Quasi vel similique, ex illo nisi mollitia. Quos, doloribus.",
+    items: [
+        {
+            Institution: "Federal University Of Technology Akure",
+            Degree: "Forestry And Wood Technology",
+            Duration: "2017-2024",
+        },
+         {
+            Company: "International College Of Safety And Management Professional",
+            Position: "Post graduate Diploma",
+            Duration: "2025",
+        },
+        {
+            Company: "Premium Smile Dental Clinic",
+            Position: "Website Developer In Information Technology",
+            Duration: "2025-2026",
+        },
+    ]
+
+}
 
 
 export default function Resume ()  {
