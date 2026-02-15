c5d022dc95a5b0dc050f71cd64e9ea0354df48b8
Bida3443 <alexanderbamise@gmail.com>
2026-02-15 10:13:26 -0800

    My Porfolio

--- app/story/page.tsx
+++ app/story/page.tsx
@@ -7,6 +7,21 @@ import HTMLFlipBook from "react-pageflip";
 export default function Story ()  {
 
     return(
-        <div>My Story</div>
-    )
+         
+    <div className="relative h-full w-full flex flex-col items-center justify-center rounded-r-xl bg-linear-to-br from-blue-900 via-black to-black text-white overflow-hidden max-w-6xl mx-auto">
+
+      {/* Subtle light overlay */}
+      <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,rgba(255,255,255,0.08),transparent_70%)]"></div>
+
+      {/* Glowing Animated Title */}
+      <h1 className="relative text-3xl md:text-4xl font-bold tracking-wide animate-pulse drop-shadow-[0_0_20px_rgba(59,130,246,0.8)]">
+        The Tech Journey
+      </h1>
+
+      <p className="relative mt-4 text-sm opacity-80">
+        By BiDa
+      </p>
+    </div>
+  );
+    
 }
