db5bd64946069e0e87b56c46a09472b3e345c5dc
Bida3443 <alexanderbamise@gmail.com>
2026-02-20 03:15:53 -0800

    My Porfolio

--- app/story/page.tsx
+++ app/story/page.tsx
@@ -17,7 +17,10 @@ export default function Story ()  {
  const book = useRef<any>(null);
 
   return (
+
+    
     <HTMLFlipBook
+    
       ref={book}
       width={400}
       height={600}
@@ -43,35 +46,31 @@ export default function Story ()  {
       className=""
       style={{}}
     >
-      <div>Cover</div>
-      <div>Page 1</div>
-      <div>Page 2</div>
+      {/* <div className="flex justify-between">
+        <div className="relative h-full w-full flex flex-col items-center justify-center rounded-r-xl bg-linear-to-br from-blue-900 via-black to-black text-white overflow-hidden max-w-6xl mx-auto">
 
-
-      <div className="relative h-full w-full flex flex-col items-center justify-center rounded-r-xl bg-linear-to-br from-blue-900 via-black to-black text-white overflow-hidden max-w-6xl mx-auto">
-
-      {/* Subtle light overlay */}
+      Subtle light overlay
       <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,rgba(255,255,255,0.08),transparent_70%)]"></div>
 
-      {/* Glowing Animated Title */}
+      Glowing Animated Title
       <h1 className="relative text-3xl md:text-4xl font-bold tracking-wide animate-pulse drop-shadow-[0_0_20px_rgba(59,130,246,0.8)]">
         The Tech Journey
       </h1>
 
       <p className="relative mt-4 text-sm opacity-80">
         By BiDa
-      </p>
-      <div>ffffff</div>
-    </div>
+      </p> */}
+      
+    {/* </div>
+      </div> */}
+      
+      <div>Cover Page</div>
+      <div>Page 1</div>
+      <div>Page 2</div>
+
     </HTMLFlipBook>
   
 
-
-
-
-         
-    
-    
   );
     
 }
