3071bf1a5f3e840c2d8958afe160c1c028d24f6f
Bida3443 <alexanderbamise@gmail.com>
2026-02-20 03:53:05 -0800

    My Porfolio

--- app/story/page.tsx
+++ app/story/page.tsx
@@ -23,12 +23,12 @@ export default function Story ()  {
     
       ref={book}
       width={400}
-      height={600}
+      height={400}
       size="stretch"
       minWidth={315}
       maxWidth={1000}
-      minHeight={400}
-      maxHeight={1536}
+      minHeight={200}
+      maxHeight={536}
       maxShadowOpacity={0.5}
       showCover
       mobileScrollSupport
@@ -56,7 +56,7 @@ export default function Story ()  {
       <h1 className="relative text-3xl md:text-4xl font-bold tracking-wide animate-pulse drop-shadow-[0_0_20px_rgba(59,130,246,0.8)]">
         The Tech Journey
       </h1>
-
+      
       <p className="relative mt-4 text-sm opacity-80">
         By BiDa
       </p> */}
@@ -64,9 +64,17 @@ export default function Story ()  {
     {/* </div>
       </div> */}
       
-      <div>Cover Page</div>
-      <div>Page 1</div>
-      <div>Page 2</div>
+      <div className="h-full flex items-center justify-center bg-blue-700 text-black p-6">Cover Page</div>
+      <div className="h-full flex items-center justify-center bg-white text-black p-6">Page 1</div>
+      <div className="h-full flex items-center justify-center bg-white text-black p-6">Page 2</div>
+      <div className="h-full flex items-center justify-center bg-white text-black p-6">Page 3</div>
+      <div className="h-full flex items-center justify-center bg-white text-black p-6">Page 4</div>
+      <div className="h-full flex items-center justify-center bg-white text-black p-6">Page 6</div>
+      <div className="h-full flex items-center justify-center bg-white text-black p-6">Page 7</div>
+      <div className="h-full flex items-center justify-center bg-white text-black p-6">Page 8</div>
+      <div className="h-full flex items-center justify-center bg-white text-black p-6">Page 9</div>
+      <div className="h-full flex items-center justify-center bg-white text-black p-6">Page 10</div>
+      <div className="h-full flex items-center justify-center bg-white text-black p-6">Page 11</div>
 
     </HTMLFlipBook>
   
